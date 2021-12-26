2e1662a24cb06778907f50da81f6f5729bf545fe
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-12-26 22:18:30 +0330

    On-borad LED blink

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 
 Create a new project using PlatformIO with the following settings:
 
-- Board: WeMos Lolin32
+- Board: WeMos LOLIN32
 - Framework: ESP-IDF (Espressif IoT Development Framework)
 
 
+## Board Pinout
+
+![](assets/esp32-lolin32.png)
--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
-void app_main() {}
+#include <driver/gpio.h>
+// Include FreeRTOS for delay
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#define LED 5 // LED connected to GPIO5 (On-board LED)
+
+int app_main() {
+    // Configure pin
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1ULL << LED);
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    // Main loop
+    while(true) {
+        gpio_set_level(LED, 0);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        gpio_set_level(LED, 1);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+}
