b3ea0905c2261a744a3b35e0f2a81b151ea9ee93
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-05-24 21:20:19 +0430

    Demo for 02 - Blinky

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Create a new project using PlatformIO with the following settings:
 ## Board Pinout
 
 ![](assets/esp32-lolin32.png)
+
+
+## Resources
+
+- [Introduction to RTOS (YouTube)](https://www.youtube.com/watch?v=F321087yYy4&list=PLEBQazB0HUyQ4hAPU1cJED6t3DU0h34bz)
+- [Introduction to RTOS (GitHub)](https://github.com/ShawnHymel/introduction-to-rtos)
--- src/main.c
+++ src/main.c
@@ -1,25 +1,64 @@
-#include <driver/gpio.h>
-// Include FreeRTOS for delay
+/**
+ * Demo for 02 - Blinky
+ *
+ * Toggles the LED on and off in its own task/thread.
+ */
+
+
+// Include FreeRTOS for delay and task functionality
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+// Include the GPIO driver for the LED
+#include <driver/gpio.h>
 
-#define LED 5 // LED connected to GPIO5 (On-board LED)
+// Use only core 1 for demo purposes
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
 
-int app_main() {
-    // Configure pin
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = (1ULL << LED);
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
+#define LED_PIN 5 // LED connected to GPIO5 (On-board LED)
 
-    // Main loop
-    while(true) {
-        gpio_set_level(LED, 0);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        gpio_set_level(LED, 1);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-    }
+// Our task: blink an LED
+void toggleLED(void *parameter)
+{
+  while (1)
+  {
+    gpio_set_level(LED_PIN, 1);
+    vTaskDelay(500 / portTICK_RATE_MS);
+    gpio_set_level(LED_PIN, 0);
+    vTaskDelay(500 / portTICK_RATE_MS);
+  }
+}
+
+int app_main()
+{
+  // Configure pin
+  gpio_config_t io_conf;
+  io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+  io_conf.mode = GPIO_MODE_OUTPUT;
+  io_conf.pin_bit_mask = (1ULL << LED_PIN);
+  io_conf.pull_down_en = 0;
+  io_conf.pull_up_en = 0;
+  gpio_config(&io_conf);
+
+  // Task to run forever
+  xTaskCreatePinnedToCore( // Use xTaskCreate() in vanilla FreeRTOS
+      toggleLED,           // Function to be called
+      "Toggle LED",        // Name of task
+      1024,                // Stack size (bytes in ESP32, words in FreeRTOS)
+      NULL,                // Parameter to pass to function
+      1,                   // Task priority (0 to configMAX_PRIORITIES - 1)
+      NULL,                // Task handle
+      app_cpu);            // Run on one core for demo purposes (ESP32 only)
+
+  // If this was vanilla FreeRTOS, you'd want to call vTaskStartScheduler() in
+  // main after setting up your tasks.
+
+  // Main loop
+  while (1)
+  {
+    // Do nothing
+  }
 }
